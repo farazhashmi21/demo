7a7ca93285be7665a807efcae30f7c266e56d7bd
farazhashmi21 <developerfaraz@yahoo.com>
2022-07-02 05:19:02 +0000

    GC Added, Remote Addition Command Removed;

--- demo.sh
+++ demo.sh
@@ -1,8 +1,9 @@
 echo "Project Name: demo";
+echo $(git gc);
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "first commit");
+echo $(git commit -m "GC Added, Remote Addition Command Removed;");
 echo $(git branch -M master);
-echo $(git remote add origin https://github.com/farazhashmi21/demo.git);
+#echo $(git remote add origin https://github.com/farazhashmi21/demo.git);
 echo $(git remote -v);
 echo $(git push -u origin master);
