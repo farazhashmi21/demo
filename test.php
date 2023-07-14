c749794ea87775c9b1c63a911797140f6ac2d4bc
farazhashmi21 <farazhashmi9000@gmail.com>
2023-07-14 14:42:54 +0000

    Quick Testing Page Styled.

--- test.php
+++ test.php
@@ -1,12 +1,44 @@
-<?php
-class Test{
-  public function __construct(){}
-  public function displ(){
-    phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
-}
-?>
-<?php
-$exec = new Test();
-$exec->displ();
-?>
+  .page-headng{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2%;
+  }
+  .bodyStyle{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: center;
+  }
+  </style>
+  </head>
+  <body class="bodyStyle">
+    <div class="page-headng">
+      <h1 style="padding: 0; !important;">Pakistan</h1>
+      <p>Zindabad</p>
+    </div>
+    <button onClick="JavaScriptObjects();">Click</button>
+    <script type="text/javascript">
+      function JavaScriptObjects(){
+        let cars = {
+          car_name: "Audi S8"
+          // run: => (){ console.log("Drive The Car!") };
+        };
+        console.log(typeof cars);
+      }
+    </script>
+</body>
+</html>
