36291d8ebc098299e8a5e0b7030b9a1c4412fafa
farazhashmi21 <farazhashmi9000@gmail.com>
2023-07-19 13:35:10 +0000

    Test File Updated

--- test.php
+++ test.php
@@ -38,9 +38,58 @@
     </div>
     <button class="mainButtonStyle" onClick="JavaScriptTestFunction();">Click</button>
     <script type="text/javascript">
+      function getUserInput(question){
+        let inputFromUser = prompt(question, null);
+        return inputFromUser;
+      }
+      function JavaScriptObjectsExample(){
+        let person = {
+          // Example Values Starts
+          // firstName: "Sara",
+          // lastName: "Ali",
+          // eyeColor: "Blue",
+          // faceCompliction: "Fair",
+          // hairColor: "Dark Brown",
+          // nationality: "Pakistani",
+          // nic: 12345678909,
+          // passport: 09876543212,
+          // mobileNumber: 03456534298,
+          // bloodGroup: "B+",
+          // cupSize: "24:36",
+          // height: 5.4,
+          // weight: 60 + "KG",
+          // age: 24
+          // Example Values Ends
+
+          firstName: this.getUserInput("Enter Your First Name Here: "),
+          lastName: this.getUserInput("Enter Your Last Name Here: "),
+          eyeColor: this.getUserInput("Enter Your Eye Color Here"),
+          faceCompliction: this.getUserInput("Enter Your Skin Color Here: "),
+          hairColor: this.getUserInput("Enter You Hari Color Here: "),
+          nationality: this.getUserInput("Enter Your Country Of Origin Here: "),
+          nic: Number(this.getUserInput("Enter Your National Identification Card Number Here: ")),
+          passport: Number(this.getUserInput("Enter Your Passport Number Here")),
+          mobileNumber: Number(this.getUserInput("Enter Your Mobile Number Here: ")),
+          bloodGroup: this.getUserInput("Enter Your Blood Group Here: "),
+          cupSize: Number(this.getUserInput("Enter your Cup Size in Case Of A Male, Enter Your Chest Size: ")),
+          height: Number(this.getUserInput("Enter Your Height Here: ")),
+          weight: this.getUserInput("Enter Your Weight Here: "),
+          age: Number(this.getUserInput("Enter Your Age Here: "))
+        };
+        console.log("Name: " + person.firstName + " " + person.lastName);
+        console.log("Eye Color: " + person.eyeColor);
+        console.log("Skin Color: " + person.faceCompliction);
+        console.log("Hair Color: " + person.hairColor);
+        console.log("NIC#: " + person.nic);
+        console.log("Mobile#: " + person.mobileNumber);
+        console.log("Height: " + person.height);
+        console.log("Weight: " + person.weight);
+        console.log("Cup Size: " + person.cupSize);
+        console.log("Blood Group#: " + person.bloodGroup);
+        console.log("Age: " + person.age);
+      }
       function JavaScriptTestFunction(){
-        c = 1;
-        console.log(typeof "WWE");
+        this.JavaScriptObjectsExample();
       }
     </script>
 </body>
