4c7ccf78b81ceb9aca7314ec7a896ee0096e57e0
farazhashmi21 <farazhashmi9000@hotmail.com>
2024-01-17 18:28:04 +0000

    Project Update

--- wp-content/themes/classichub/footer.php
+++
@@ -1,7 +0,0 @@
-    <footer class="main-footer">
-        <p><?php bloginfo("name") ?> - &#169; - <script type="text/javascript">document.write(new Date().getFullYear()); </script></p>
-        <?php wp_nav_menu(array("theme_location" => "footer_menu", "menu_class" => "navi")); ?>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
