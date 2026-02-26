deefae46e6f0605f5bc60c118f3e759cc132f96d
farazhashmi21 <farazhashmi9000@yahoo.com>
2026-02-26 19:43:46 +0000

    DB changed;

--- demo/settings.py
+++ demo/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'demodb',
-        'USER': 'developerfarazhashmi',
-        'PASSWORD': '@tCMSuIm!b0.x.9I',
-        'HOST': 'localhost',  # Or the IP address/hostname of your MySQL server
-        'PORT': '3306',       # The default MySQL port
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'demodb',
+#         'USER': 'developerfarazhashmi',
+#         'PASSWORD': '@tCMSuIm!b0.x.9I',
+#         'HOST': 'localhost',  # Or the IP address/hostname of your MySQL server
+#         'PORT': '3306',       # The default MySQL port
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "hello/static")
 ]
