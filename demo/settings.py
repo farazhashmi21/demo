a11b9859c310a4d8ee1e251081f3a779c790cd03
farazhashmi21 <farazhashmi9000@yahoo.com>
2026-02-26 18:18:52 +0000

    Docker INITIALIZE;

--- demo/settings.py
+++ demo/settings.py
@@ -78,10 +78,25 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demodb',
+        'USER': 'developerfarazhashmi',
+        'PASSWORD': '@tCMSuIm!b0.x.9I',
+        'HOST': 'localhost',  # Or the IP address/hostname of your MySQL server
+        'PORT': '3306',       # The default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
