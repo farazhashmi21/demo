4a3efd2d2b48f181fd3df8af5535ab887a714c40
farazhashmi21 <farazhashmi9000@yahoo.com>
2026-03-02 13:36:28 +0000

    More VIEWS developed;

--- hello/views.py
+++ hello/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'hello/index.html')
+
+
+def about(request):
+    return render(request, 'hello/about.html')
+
+
+def services(request):
+    return render(request, 'hello/services.html')
+
+
+def contact(request):
+    return render(request, 'hello/contact.html')
